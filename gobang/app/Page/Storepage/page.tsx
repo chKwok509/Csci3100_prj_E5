399fa2b746533d70b3a481ac35138b69db78a09e
cliff1679 <cailloulee17@gmail.com>
2024-04-09 20:09:11 +0800

    Store UI update
    better UI for store, prep buy functions

--- gobang/app/Page/Storepage/page.tsx
+++ gobang/app/Page/Storepage/page.tsx
@@ -3,22 +3,29 @@ import styles from './storepage.module.css';
 import Image from 'next/image';
 import mainimage from '../../../public/Store.png';
 import playericon from '../../../public/meme.png';
+import skin from '../Storepage/skin/skin1.svg'
 
 
 export default function StorePage() {
-    return (
-        <body className={styles.body}>
-            <div className={styles.center_image}>
-                <Image src={mainimage} alt="Main Image" className={styles.center_image_img} />
-            </div>
-            
-    <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to the Store</h1>
-      <div className={styles.imageContainer}>
+  return (
+    <body className={styles.body}>
+      <div className={styles.center_image}>
+        <Image src={mainimage} alt="Main Image" className={styles.center_image_img} />
       </div>
-      <p className={styles.description}>Browse and purchase items from our store.</p>
-      {/* Add your store content here */}
-    </div>
+      <div className={styles.container}>
+        <div className={styles.boxRow}>
+          <div className={styles.box}><Image src={skin} alt="SVG Image" className={styles.skins}/></div>
+          <div className={styles.box}></div>
+          <div className={styles.box}></div>
+          <div className={styles.box}></div>
+        </div>
+        <div className={styles.boxRow}>
+          <div className={styles.box}></div>
+          <div className={styles.box}></div>
+          <div className={styles.box}></div>
+          <div className={styles.box}></div>
+        </div>
+      </div>
     </body>
   );
 }
