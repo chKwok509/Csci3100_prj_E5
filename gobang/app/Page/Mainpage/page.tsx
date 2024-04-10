5240225a6d4aa4c9e55f017b64690c81e5e8c60a
cliff1679 <cailloulee17@gmail.com>
2024-04-10 23:26:01 +0800

    Update page.tsx

--- gobang/app/Page/Mainpage/page.tsx
+++ gobang/app/Page/Mainpage/page.tsx
@@ -14,25 +14,25 @@ export default function MainPage() {
     return (
         <body className={styles.body}>
             <div className={styles.center_image}>
-                <Image src={mainimage} alt="Main Image" className={styles.center_image_img} width={600} height={120}/>
-                </div>
+                <Image src={mainimage} alt="Main Image" className={styles.center_image_img} width={600} height={120} />
+            </div>
             <div className={styles.playerInfo}>
-                <Image src={playericon} alt="Player Icon" className={styles.playerInfo_img} width={600} height={120}/>
+                <Image src={playericon} alt="Player Icon" className={styles.playerInfo_img} width={600} height={120} />
                 <div>
                     <p className={styles.playerID}>ID: 12345</p>
                     <p className={styles.playerName}>Wilson Lui</p>
                 </div>
             </div>
-        <br></br>
-        <button className={styles.button} onClick={() => window.location.href = '/Page/Gamepage'}>PVP</button>
-        <button className={styles.button} >PVC</button>
-        
+            <br></br>
+            <button className={styles.button} onClick={() => window.location.href = '/Page/Gamepage'}>PVP</button>
+            <button className={styles.button} >PVC</button>
+
             <button className={styles.button} onClick={() => router.push('/Page/Storepage')}>Store</button>
             <button className={styles.button} onClick={() => router.push('/Page/Communitypage')}>Community</button>
-        
-        <button className={styles.button} onClick={() => confirm("Quit?") && router.push('/')}>Quit</button>
-        <div className={styles.version}>V0.0.0</div>
-        <Chatroom/>
+
+            <button className={styles.button} onClick={() => confirm("Quit?") && router.push('/')}>Quit</button>
+            <div className={styles.version}>V0.0.0</div>
+            <Chatroom />
         </body>
     );
 }
